03b10c208f8502ffef3de3e9cc0309aaab8c2a2e
Jinseonghyun <jinsunghyun9@gmail.com>
2024-09-02 08:49:56 +0900

    프로젝트 세팅

---
+++ src/main/java/jin/redisluascript/RedisLuaScriptApplication.java
@@ -0,0 +1,13 @@
+package jin.redisluascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLuaScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLuaScriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jin/redisluascript/RedisLuaScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package jin.redisluascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLuaScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
